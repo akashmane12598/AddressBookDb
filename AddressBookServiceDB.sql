11467d940892f68c36b4c20d199e6d2622fb2f4b
Akash <akashmane12598@gmail.com>
2020-11-01 19:16:36 +0530

    [Akash] Address Book SQL table Service From UC1 to UC12

--- AddressBookServiceDB.sql
+++ AddressBookServiceDB.sql
@@ -58,6 +58,79 @@ insert into AddressBook values
 ('Akhil','Shah','Mumbai','Thane','Maharashtra',400604,'2013456789','akhil@gmail.com','Book1','Family'),
 ('Akhil','Shah','Mumbai','Thane','Maharashtra',400604,'2013456789','akhil@gmail.com','Book2','Friends');
 
-
-
 select * from AddressBook;
+
+
+/* UC12 */
+/* ER diagram */
+
+/* create table BookNameType */
+ create table BookNameType(
+ B_ID varchar(50) not null primary key,
+ B_Name varchar(50) not null,
+ B_Type varchar(50) not null,
+ );
+
+
+ /* insert data */
+ insert into BookNameType values
+ ('BK1','Book1','Family'),
+ ('BK2','Book2','Friends'),
+ ('BK3','Book3','Profession');
+
+select * from BookNameType;
+
+
+
+/*create table Contacts*/
+create table Contacts(
+FirstName varchar(50) not null,
+LastName varchar(50) not null,
+Address varchar(50) not null,
+City varchar(50) not null,
+State varchar(50) not null,
+Zip int not null,
+PhoneNumber varchar(50) not null,
+Email varchar(50) not null,
+B_ID varchar(50) not null,
+foreign key(B_ID) references BookNameType(B_ID)
+);
+
+
+/* Insert data */
+insert into Contacts values
+('Akash','Mane','Mumbai','Mulund','Maharashtra',400080,'1234567890','akash@gmail.com','BK1'),
+('Tanmay','Maity','Kolkatta','Howrah','Bengal',600080,'0123456789','tanmay@gmail.com','BK2'),
+('Parth','Trivedi','Rajkot','Jaamnagar','Gujarat',800080,'1023456789','parth@gmail.com','BK2'),
+('Yash','Mane','Mumbai','Mulund','Maharashtra',400080,'1203456789','yash@gmail.com','BK1'),
+('Chinmay','Pimple','Mumbai','Borivali','Maharashtra',800080,'1234506789','chinmay@gmail.com','BK3'),
+('Akhil','Shah','Mumbai','Thane','Maharashtra',400604,'2013456789','akhil@gmail.com','BK1'),
+('Akhil','Shah','Mumbai','Thane','Maharashtra',400604,'2013456789','akhil@gmail.com','BK2');
+
+
+select * from Contacts;
+
+
+/* UC13 */
+/*  Retrive data from table */
+select CONCAT(C.FirstName,' ', C.LastName) as PersonName, C.City, C.State from Contacts C inner join BookNameType B
+on C.B_ID=B.B_ID 
+where C.City='Mulund' or C.State='Bengal';
+
+
+select CONCAT(C.FirstName,' ', C.LastName) as PersonName, C.City, count(C.FirstName) as TotalContactsInCity from Contacts C inner join BookNameType B
+on C.B_ID=B.B_ID 
+group by(C.City),(CONCAT(C.FirstName,' ', C.LastName))
+order by PersonName;
+
+
+select CONCAT(C.FirstName,' ', C.LastName) as PersonName, C.State, count(C.FirstName) as TotalContactsInCity from Contacts C inner join BookNameType B
+on C.B_ID=B.B_ID 
+group by(C.State), CONCAT(C.FirstName,' ', C.LastName)
+order by PersonName;
+
+
+select B.B_Type as Types, COUNT(B.B_Type) as Total from Contacts C inner join BookNameType B
+on C.B_ID=B.B_ID 
+group by(B.B_Type);
+
